2b2df81cf52f96c6860b238eccbc0995c89ad749
Yechen Wei <yew019@ucsd.edu>
2020-02-26 19:03:21 -0800

    a

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'hfOfTAX2sAAQGL447iFx5F4oQ375D6NP';
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.88, -117.236]).addTo(map);
 }
